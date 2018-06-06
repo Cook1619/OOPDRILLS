9fe26c7fa189d50022354b733285506b7111508d
Cook1619 <cook6348@gmail.com>
2018-06-06 16:50:44 -0500

    Started making my class the new way

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 
-function Person (name, city, age){
-    this.name = name;
-    this.city = city;
-    this.age = age;
-}
+// function Person (name, city, age){
+//     this.name = name;
+//     this.city = city;
+//     this.age = age;
+// }
 
-Person.prototype.greet = function(){
-    alert(`Hey! My name is ${this.name} I am ${this.age} years old and live in ${this.city}.`)
-}
+// Person.prototype.greet = function(){
+//     alert(`Hey! My name is ${this.name} I am ${this.age} years old and live in ${this.city}.`)
+// }
 
-let person1 = new Person('Danelle', 'Mayer', 32);
-console.log(person1.greet());
+// let person1 = new Person('Danelle', 'Mayer', 32);
+// console.log(person1.greet());
 
-let person2 = new Person('Mike', 'Montrose', 30);
-console.log(person2.greet());
+// let person2 = new Person('Mike', 'Montrose', 30);
+// console.log(person2.greet());
 
-let person3 = new Person('Aaron', 'Cedar City', 29);
-console.log(person3.greet());
+// let person3 = new Person('Aaron', 'Cedar City', 29);
+// console.log(person3.greet());
 
-let person4 = new Person('Ahmed', 'Plymouth', 31);
-console.log(person4.greet());
-let person5 = new Person('Mike', 'Albertville', 31);
-console.log(person5.greet());
+// let person4 = new Person('Ahmed', 'Plymouth', 31);
+// console.log(person4.greet());
+// let person5 = new Person('Mike', 'Albertville', 31);
+// console.log(person5.greet());
 
 
 
